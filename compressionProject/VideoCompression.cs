8efb0a850135a9daca43e95948edfeec6c412483
Robert Moffat <robertnmoffat@gmail.com>
2016-03-07 20:56:00 -0800

    pushing changes

---
+++ compressionProject/VideoCompression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compressionProject
+{
+    class VideoCompression
+    {
+        Block errorBlock;
+
+        public Point vector(Bitmap image, Block comparison, int x, int y, int range) {
+            
+        } 
+    }
+}
